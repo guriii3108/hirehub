c9cad65a8987aa8b0a26d6caa34346131e5e79e7
Gurveer Singh <sgurveer97@gmail.com>
2026-01-13 14:10:59 +0530

    navbar UI + supabase setup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button } from './ui/button'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+      <nav className='py-4 flex justify-between items-center'>
+        <Link to="/">
+          <img src="/logo.svg" className='h-20' />
+        </Link>
+        <div className="flex gap-4">
+          <Button variant="outline">Login</Button>
+        </div>
+      </nav>
+    </>
   )
 }
 
