fef5b5482f9a3a08147fd54910e4e6c4b922b76b
Gurveer Singh <sgurveer97@gmail.com>
2026-01-13 13:39:38 +0530

    basic pages setuo

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import AppLayout from './layouts/AppLayout'
 import LandingPage from './pages/LandingPage'
+import OnBoarding from './pages/OnBoarding'
+import JobListing from './pages/JobListing'
+import Job from './pages/Job'
+import PostJob from './pages/PostJob'
+import SavedJobs from './pages/SavedJobs'
 
 const router = createBrowserRouter([
   {
@@ -10,7 +15,28 @@ const router = createBrowserRouter([
       {
         path:"/",
         element:<LandingPage />
-      }
+      },
+      {
+        path:"/onboarding",
+        element:<OnBoarding />
+      },
+      {
+        path:"/jobs",
+        element:<JobListing />
+      },
+      {
+        path:"/job/:id",
+        element:<Job />
+      },
+      {
+        path:"/post-job",
+        element:<PostJob />
+      },
+      {
+        path:"/saved-job",
+        element:<SavedJobs />
+      },
+
     ]
   }
 ])
