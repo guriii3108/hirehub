69f7b292ec04fef696d77e70e7181d892b11e653
Gurveer Singh <sgurveer97@gmail.com>
2026-01-13 13:28:24 +0530

    added components (accordian,card,drawer,input,label,radio-group,select,text-area,carousel)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Button } from './components/ui/button'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Button variant="outline">Button</Button>
+    </div>
   )
 }
 
