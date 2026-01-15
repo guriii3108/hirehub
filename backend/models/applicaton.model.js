5bb1e2ab87318c528ca06655bf80e284004a9a4a
Gurveer Singh <sgurveer97@gmail.com>
2026-01-15 13:08:51 +0530

    application schema

---
+++ backend/models/applicaton.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const applicationSchema = new mongoose.Schema({ //this is from applicant... the one who filled the job application form
+  job:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Job",
+    required: true,
+  },
+  applicant:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  status:{
+    type: String,
+    enum: ["pending", "accepted", "rejected"],
+    default: "pending"
+  },
+  resume:{
+    type: String,
+  },
+},{timestamps: true})
+
+const Application = mongoose.model("Application", applicationSchema);
+export default Application;
