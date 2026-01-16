6391680e4256550966626d6bb0568d3dec342b83
Gurveer Singh <sgurveer97@gmail.com>
2026-01-16 17:40:08 +0530

    middleware in company related routes

--- backend/routes/company.route.js
+++ backend/routes/company.route.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { getCompany, getCompanyById, registerCompany, updateCompany } from "../controllers/company.controller.js";
-import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { isAuthenticated } from "../middlewares/auth.middleware.js";
 const route = Router();
 
 //company routes
-route.post("/register",authMiddleware,registerCompany);
-route.get("/get-company",authMiddleware,getCompany);
-route.get("/get-company/:id",authMiddleware,getCompanyById);
-route.put("/update-company/:id",authMiddleware,updateCompany);
+route.post("/register",isAuthenticated,registerCompany);
+route.get("/get-company",isAuthenticated,getCompany);
+route.get("/get-company/:id",isAuthenticated,getCompanyById);
+route.put("/update-company/:id",isAuthenticated,updateCompany);
 
 
 export default route;
