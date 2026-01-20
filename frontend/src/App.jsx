b6b283308b9c22738ca9c3af1c251a1001059c75
Gurveer Singh <sgurveer97@gmail.com>
2026-01-20 11:19:00 +0530

    basic routing setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import SignUp from './pages/SignUp.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const App = () => {
+  const appRouter = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />
+    },
+    {
+      path: '/login',
+      element: <Login />
+    },
+    {
+      path: '/signup',
+      element: <SignUp />
+    }
+  ])
   return (
-    <div>
-      <Navbar />
-    </div>
+    <>
+    <RouterProvider router={appRouter} />
+    </>
   )
 }
 
