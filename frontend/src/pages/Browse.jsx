066acb8a94b3fc040c2adf5ee8211a59e69e2a4e
Gurveer Singh <sgurveer97@gmail.com>
2026-01-24 23:46:42 +0530

    browse base template()navbar + footer

---
+++ frontend/src/pages/Browse.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import Job from '../components/Job'
+
+const Browse = () => {
+  return (
+    <div>
+      <Navbar/>
+      <div className='max-w-7xl mx-auto bg-gray-50/50 min-h-screen mt-5'>
+       BROWSE PAGE
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default Browse
