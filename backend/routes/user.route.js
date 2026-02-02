004bd635bf3025eff15a680bdf01452f2303eef1
Gurveer Singh <sgurveer97@gmail.com>
2026-02-02 23:32:39 +0530

    update user profile

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,14 +1,19 @@
 import express from "express";
-import { register, login, logout, updateProfile } from "../controllers/user.controller.js";
+import {
+    register,
+    login,
+    logout,
+    updateProfile,
+} from "../controllers/user.controller.js";
 import { Router } from "express";
 import { isAuthenticated } from "../middlewares/auth.middleware.js";
 import { singleUpload } from "../middlewares/multer.js";
 
 const route = Router();
 
-route.post("/register",singleUpload, register);
+route.post("/register", singleUpload, register);
 route.post("/login", login);
-route.post("/logout", isAuthenticated ,logout);
-route.put("/update", isAuthenticated ,updateProfile);
+route.post("/logout", isAuthenticated, logout);
+route.put("/update", isAuthenticated, singleUpload, updateProfile);
 
 export default route;
