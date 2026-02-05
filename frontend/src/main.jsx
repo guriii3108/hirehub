4e4a4b6e887d87b3435fc523ed5ccae54f5af1e5
Gurveer Singh <sgurveer97@gmail.com>
2026-02-05 22:02:28 +0530

    redux-persist(to stop unwanted refresh data loss)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,15 @@ import App from './App.jsx'
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 import { Provider } from 'react-redux';
-import store from "./redux/store.js";
+import store, { persistor } from "./redux/store.js";
+import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')).render(
     <>
         <Provider store={store}>
-            <App />
+            <PersistGate loading={null} persistor={persistor}>
+                <App />
+            </PersistGate>
             <ToastContainer />
         </Provider>
     </>
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,51 @@
-import {configureStore} from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
+import {
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage"; //default storage is local storage
+import { persistStore } from "redux-persist";
+
+const persistConfig = {
+    key: "root", //key under which data will be stored in local storage
+    storage,
+    //why these?
+    //FLUSH: when we dispatch an action, it flushes the state to storage
+    //REHYDRATE: when we refresh the page, it rehydrates the state from storage
+    //PAUSE: when we pause the app, it pauses the state to storage
+    //PERSIST: when we persist the app, it persists the state to storage
+    //PURGE: when we purge the app, it purges the state from storage
+    //REGISTER: when we register the app, it registers the state to storage
+};
+
+//wrap the reducer with persistReducer
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
 
 const store = configureStore({
-  reducer:{
-    //we pass slices here(umm means diff diff fields.. like user, job etc)
-    auth: authReducer
-  }
-})
+    reducer: {
+        //we pass slices here(umm means diff diff fields.. like user, job etc)
+        auth: persistedAuthReducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [
+                    FLUSH,
+                    REHYDRATE,
+                    PAUSE,
+                    PERSIST,
+                    PURGE,
+                    REGISTER,
+                ],
+            },
+        }),
+});
 
+export const persistor = persistStore(store);
 export default store;
