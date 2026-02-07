c56f902e2c318547aef427761db311dd4920cc78
Gurveer Singh <sgurveer97@gmail.com>
2026-02-07 22:07:10 +0530

    custom hook to get company By Id

---
+++ frontend/src/hooks/useGetCompanyById.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import axios from "axios";
+import { COMPANY_API_ENDPOINT} from "../utils/constant";
+import { useDispatch } from "react-redux";
+import { setSingleCompany } from "../redux/companySlice";
+import { showError } from "../utils/toast";
+
+const useGetCompanyById = (id) => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const fetchSingleCompany =async()=>{
+      try{
+        const response = await axios.get(`${COMPANY_API_ENDPOINT}/get-company/${id}`,{
+          withCredentials:true,
+        });
+        if(response.data.success){
+          dispatch(setSingleCompany(response.data.company))
+        }
+      }catch(error){
+        showError(error.response.data.message);
+      }
+    }
+    fetchSingleCompany();
+  }, [id,dispatch])
+}
+
+export default useGetCompanyById;
