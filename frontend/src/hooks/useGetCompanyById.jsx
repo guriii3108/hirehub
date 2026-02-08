c563f85f5d60ba526e9da3b248aaf966270a50bc
Gurveer Singh <sgurveer97@gmail.com>
2026-02-08 23:18:40 +0530

    custom hook(changes)

--- frontend/src/hooks/useGetCompanyById.jsx
+++ frontend/src/hooks/useGetCompanyById.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import axios from "axios";
-import { COMPANY_API_ENDPOINT} from "../utils/constant";
+import { COMPANY_API_ENDPOINT } from "../utils/constant";
 import { useDispatch } from "react-redux";
 import { setSingleCompany } from "../redux/companySlice";
 import { showError } from "../utils/toast";
@@ -8,20 +8,21 @@ import { showError } from "../utils/toast";
 const useGetCompanyById = (id) => {
   const dispatch = useDispatch();
   useEffect(() => {
-    const fetchSingleCompany =async()=>{
-      try{
-        const response = await axios.get(`${COMPANY_API_ENDPOINT}/get-company/${id}`,{
-          withCredentials:true,
+    if(!id) return;
+    const fetchSingleCompany = async () => {
+      try {
+        const response = await axios.get(`${COMPANY_API_ENDPOINT}/get-company/${id}`, {
+          withCredentials: true,
         });
-        if(response.data.success){
+        if (response.data.success) {
           dispatch(setSingleCompany(response.data.company))
         }
-      }catch(error){
+      } catch (error) {
         showError(error.response.data.message);
       }
     }
     fetchSingleCompany();
-  }, [id,dispatch])
+  }, [id, dispatch])
 }
 
 export default useGetCompanyById;
