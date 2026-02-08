d33c2636370596acd2e8ffd2a5b3c14e876edf74
Gurveer Singh <sgurveer97@gmail.com>
2026-02-08 23:20:03 +0530

    company slice+ index.js changes

--- backend/index.js
+++ backend/index.js
@@ -12,21 +12,25 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"]
-}));
+const corsOptions = {
+    origin: [
+        process.env.FRONTEND_URL,
+        "http://localhost:5173",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+};
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 //v1.. v2.. and on soo on .. it's called versioning for future.. if we make changes in api we can create new version
-app.use("/api/v1/user",userRoute); //user routes
-app.use("/api/v1/company",companyRoute); //company routes
-app.use("/api/v1/job",jobRoute); //job routes
-app.use("/api/v1/application",applicationRoute); //application routes
+app.use("/api/v1/user", userRoute); //user routes
+app.use("/api/v1/company", companyRoute); //company routes
+app.use("/api/v1/job", jobRoute); //job routes
+app.use("/api/v1/application", applicationRoute); //application routes
 
 app.listen(PORT, () => {
     connectDB();
--- frontend/src/redux/companySlice.js
+++ frontend/src/redux/companySlice.js
@@ -3,12 +3,12 @@ import { createSlice } from "@reduxjs/toolkit";
 const companySlice =createSlice({
   name:"company",
   initialState:{
-    companies:[], //for all companies
+    // companies:[], //for all companies
     singleCompany:null, //for single company
   },
   reducers:{
     //actions here
-    setSingleCompany:(state,action)=>{ //for single company
+      setSingleCompany:(state,action)=>{ //for single company
       state.singleCompany = action.payload;
     } 
   }
