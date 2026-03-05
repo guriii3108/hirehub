f0b1c9a27ef36d4c55d7fea0982929f831bbdb77
Gurveer Singh <sgurveer97@gmail.com>
2026-03-05 12:44:45 +0530

    feat: Add application slice to Redux store and implement applicant data fetching in the Admin Applicants component.

--- frontend/src/components/Admin/Applicants.jsx
+++ frontend/src/components/Admin/Applicants.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ApplicantsTable from './ApplicantsTable'
 import Navbar from '../Navbar'
+import { showError, showSuccess } from '../../utils/toast'
+import axios from 'axios'
+import { APPLICATION_API_ENDPOINT } from '../../utils/constant'
+import { useParams } from 'react-router-dom'
 
 const Applicants = () => {
+  const { jobId } = useParams();
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    const fetchApplicants = async () => {
+      try {
+        setLoading(true)
+        const response = await axios.get(`${APPLICATION_API_ENDPOINT}/applicants/${jobId}`, 
+          { withCredentials: true }
+        )
+        if (response?.data?.success) {
+          showSuccess(response?.data?.message || "Applicants retrieved successfully")
+          //store in redux
+          console.log(response?.data?.job?.applications)
+        }
+      } catch (error) {
+        showError(error?.response?.data?.message || "Failed to retrieve applicants")
+      }
+      finally {
+        setLoading(false)
+      }
+    }
+    fetchApplicants()
+  }, [])
   return (
     <div className='min-h-screen bg-gray-50 flex flex-col'>
       <Navbar />
