388007e35724818caca07525ec2b4bb082d65eea
Gurveer Singh <sgurveer97@gmail.com>
2026-03-05 13:12:06 +0530

    feat: Implement Redux for managing applicant state and dynamically displaying the applicant count.

--- frontend/src/components/Admin/Applicants.jsx
+++ frontend/src/components/Admin/Applicants.jsx
@@ -5,10 +5,16 @@ import { showError, showSuccess } from '../../utils/toast'
 import axios from 'axios'
 import { APPLICATION_API_ENDPOINT } from '../../utils/constant'
 import { useParams } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { setApplicants } from '../../redux/applicationSlice'
+import { useSelector } from 'react-redux'
 
 const Applicants = () => {
+  const {applicants} = useSelector((store) => store.application)
   const { jobId } = useParams();
   const [loading, setLoading] = useState(false)
+  const dispatch = useDispatch()
+
   useEffect(() => {
     const fetchApplicants = async () => {
       try {
@@ -19,7 +25,7 @@ const Applicants = () => {
         if (response?.data?.success) {
           showSuccess(response?.data?.message || "Applicants retrieved successfully")
           //store in redux
-          console.log(response?.data?.job?.applications)
+          dispatch(setApplicants(response?.data?.job?.applications))
         }
       } catch (error) {
         showError(error?.response?.data?.message || "Failed to retrieve applicants")
@@ -35,7 +41,7 @@ const Applicants = () => {
       <Navbar />
       <div className='flex-1 max-w-7xl w-full mx-auto my-10 px-4'>
         <div className="mb-6 flex items-center justify-between">
-          <h1 className='text-2xl font-bold text-gray-900'>Applicants (3)</h1>
+          <h1 className='text-2xl font-bold text-gray-900'> {applicants?.length} Applicants</h1>
         </div>
         <div className="bg-white rounded-2xl shadow-sm border border-gray-100 overflow-hidden">
           <ApplicantsTable />
